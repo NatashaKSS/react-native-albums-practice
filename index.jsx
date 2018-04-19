69250a82f21606e1adee3ba8f3b239dfd03ea8bb
Natasha Koh <NatashaKSS@users.noreply.github.com>
2018-04-20 00:59:30 +0800

    Use airbnb eslint config instead

---
+++ index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { AppRegistry, Text } from 'react-native';
+
+const App = () => (
+  <Text>Hello world!</Text>
+);
+
+AppRegistry.registerComponent('albums', () => App);
